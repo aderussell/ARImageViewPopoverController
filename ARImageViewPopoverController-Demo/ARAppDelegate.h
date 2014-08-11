fb478df78dee098f6fdd63f2d6f5096083ce7fda
aderussell <adrianrussell@me.com>
2014-08-11 20:38:02 +0100

    Initial Commit

---
+++ ARImageViewPopoverController-Demo/ARAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ARAppDelegate.h
+//  ARImageViewPopoverController-Demo
+//
+//  Created by Adrian Russell on 11/08/2014.
+//  Copyright (c) 2014 Adrian Russell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ARAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ARImageViewPopoverController-DemoTests/ARImageViewPopoverController_DemoTests.m
@@ -0,0 +1,34 @@
+//
+//  ARImageViewPopoverController_DemoTests.m
+//  ARImageViewPopoverController-DemoTests
+//
+//  Created by Adrian Russell on 11/08/2014.
+//  Copyright (c) 2014 Adrian Russell. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ARImageViewPopoverController_DemoTests : XCTestCase
+
+@end
+
+@implementation ARImageViewPopoverController_DemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
